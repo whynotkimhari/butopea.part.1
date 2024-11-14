d91017497b74970d493d7553d0fdc51030091f16
whynotkimhari <htlulem185@gmail.com>
2024-11-14 19:46:23 +0100

    Allow to access by column name

--- db.py
+++ db.py
@@ -1,6 +1,6 @@
 import sqlite3
 
-def fetch(DB_PATH: str, query: str) -> list[tuple[str]]:
+def fetch(DB_PATH: str, query: str) -> list[tuple[dict]]:
   """
     Fetch the data with given query from the given database.
     Return a list of rows. Each row is a tuple.
@@ -10,9 +10,13 @@ def fetch(DB_PATH: str, query: str) -> list[tuple[str]]:
     query: user query
   """
   conn = sqlite3.connect(DB_PATH)
+
+  # Allow to 'dictionarize' the row
+  conn.row_factory = sqlite3.Row
   cursor = conn.cursor()
+
   cursor.execute(query)
   rows = cursor.fetchall()
   conn.close()
 
-  return rows
+  return [dict(row) for row in rows]
